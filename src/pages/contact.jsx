4ffd7469c899ffdf7d15f6570d613600c134c134
jodi <128635337+jodi-j@users.noreply.github.com>
2025-01-02 22:43:09 -0500

    simple connect page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -40,36 +40,20 @@ const Contact = () => {
 
 					<div className="contact-container">
 						<div className="title contact-title">
-							Let's Get in Touch: Ways to Connect with Me
+							Connect with Me!
 						</div>
 
 						<div className="subtitle contact-subtitle">
-							Thank you for your interest in getting in touch with
-							me. I welcome your feedback, questions, and
-							suggestions. If you have a specific question or
-							comment, please feel free to email me directly at
-							&nbsp;{" "}
+							Thank you for your interest in connecting with
+							me. I am eager to receive all feedback, questions, and
+							inquiries. Please feel free to email me directly at
+							&nbsp;{""}
 							<a href={`mailto:${INFO.main.email}`}>
 								{INFO.main.email}
 							</a>
-							. I make an effort to respond to all messages within
-							24 hours, although it may take me longer during busy
-							periods. Alternatively, you can use the contact form
-							on my website to get in touch. Simply fill out the
-							required fields and I'll get back to you as soon as
-							possible. Finally, if you prefer to connect on
-							social media, you can find me on{" "}
-							<a
-								href={INFO.socials.instagram}
-								target="_blank"
-								rel="noreferrer"
-							>
-								{INFO.socials.instagram}
-							</a>
-							. I post regular updates and engage with my
-							followers there, so don't hesitate to reach out.
-							Thanks again for your interest, and I look forward
-							to hearing from you!
+							. I make an effort to respond to all messages as soon as possible.
+							Since I am both a student and intern, however, response times may vary.
+							I look forward to hearing from you!
 						</div>
 					</div>
 
