552c4e4ac368ce68af1cff3252bdd8d2ef6f51d6
jodi <128635337+jodi-j@users.noreply.github.com>
2025-01-03 12:50:22 -0500

    update resume

--- src/data/user.js
+++ src/data/user.js
@@ -9,7 +9,7 @@ const INFO = {
 	socials: {
 		github: "https://github.com/jodi-j",
 		linkedin: "https://linkedin.com/in/jodi-joven-076096240",
-		resume: "https://drive.google.com/file/d/1a-iygeNOmtYw9JnmmwCSNoy0r3xJRh1g/view?usp=sharing",
+		resume: "https://drive.google.com/file/d/1JkJB26nCi4-x07_vr4wrtGQ6K0hpafuS/view?usp=sharing",
 	},
 
 	homepage: {
